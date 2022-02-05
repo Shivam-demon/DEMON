58f63b64568c2f50547794fd7ae5bed73d073b89
Shivam-demon <98231889+Shivam-demon@users.noreply.github.com>
2022-02-05 16:41:26 +0530

    Update cricketscore.py

--- Demon/modules/cricketscore.py
+++ Demon/modules/cricketscore.py
@@ -2,7 +2,7 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from galaxinaBot import telethn as tbot
+from Demon import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
