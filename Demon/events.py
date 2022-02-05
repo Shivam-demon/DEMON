e48c7868924847480e2eba0315e6797e247120b9
Shivam-demon <98231889+Shivam-demon@users.noreply.github.com>
2022-02-05 18:45:42 +0530

    Update events.py

--- Demon/events.py
+++ Demon/events.py
@@ -145,7 +145,7 @@ def bot(**args):
     return decorator
 
 
-def Zaid(**args):
+def Demon(**args):
     pattern = args.get("pattern", None)
     disable_edited = args.get("disable_edited", False)
     ignore_unsafe = args.get("ignore_unsafe", False)
