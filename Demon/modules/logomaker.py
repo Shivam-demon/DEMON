8dc2df340ecc4821cb8658acafc49dac6d5d958b
Shivam-demon <98231889+Shivam-demon@users.noreply.github.com>
2022-06-04 09:58:50 +0530

    Update logomaker.py

--- Demon/modules/logomaker.py
+++ Demon/modules/logomaker.py
@@ -52,7 +52,7 @@ async def lego(event):
             (x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow"
         )
 
-        fname2 = "LogoByShasa.png"
+        fname2 = "LogoByDemon.png"
 
         img.save(fname2, "png")
 
